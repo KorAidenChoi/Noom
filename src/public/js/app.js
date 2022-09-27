98c988d0507ceb7b5265e7a3663c972bb75e6166
KorAidenChoi <kor.aiden.choi@gmail.com>
2022-09-26 20:31:36 -0700

    websockets are very complicated

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-js;
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server😎")
+});
+
+socket.addEventListener("message",(message) => {
+    console.log("New message:",message.data)
+});
+
+socket.addEventListener("close", () => {
+    console.log("DisConnected from server 🔉")
+});
+
+setTimeout (() => {
+    socket.send("hello from the browser!")
+},10000);

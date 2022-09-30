a46fe1254dad0b84317592ea12261aee2aa344b8
KorAidenChoi <kor.aiden.choi@gmail.com>
2022-09-29 20:16:46 -0700

    chat is fun... not

--- src/server.js
+++ src/server.js
@@ -20,10 +20,18 @@ const sockets = [];
 
 wss.on("connection",(socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anonyous"
     console.log("Connected to Browser");
     socket.on("close", () => console.log("Disconnected from Browser 😪"))
-    socket.on ("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString("utf-8")));
+    socket.on ("message", (msg) => {
+        const message = JSON.parse(msg);
+
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
     });
 });
 

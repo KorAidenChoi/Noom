d86fc431f55a94ffb6a68484ab59cdf6f0e1fd3b
KorAidenChoi <kor.aiden.choi@gmail.com>
2022-10-04 19:55:20 -0700

    stinky

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket, { WebSocketServer } from "ws";
+import SocketIO from "socket.io"
 import express from "express";
 
 const app = express();
@@ -13,26 +13,32 @@ app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
+wsServer.on("connection", (socket) => {
+    console.log("daddy is stinky");
+})
 
-wss.on("connection",(socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anonyous"
-    console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from Browser 😪"))
-    socket.on ("message", (msg) => {
-        const message = JSON.parse(msg);
 
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-});
 
-server.listen(3000, handleListen);
+//const sockets = [];
+
+// wss.on("connection",(socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anonyous"
+//     console.log("Connected to Browser");
+//     socket.on("close", () => console.log("Disconnected from Browser 😪"))
+//     socket.on ("message", (msg) => {
+//         const message = JSON.parse(msg);
+
+//         switch (message.type) {
+//             case "new_message":
+//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//         }
+//     });
+// });
+
+httpServer.listen(3000, handleListen);

b1a73792267798cf8545a1adeb41b59303e7c6d0
KorAidenChoi <kor.aiden.choi@gmail.com>
2022-10-24 20:50:28 -0700

    admin

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIO from "socket.io"
+import { Server } from "socket.io";
+import{ instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -14,7 +15,15 @@ app.get("/", (req, res) => res.render("home"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
+});
+instrument(wsServer, {
+    auth: false,
+  });
 
 function publicRooms() {
     const {sockets: {adapter: {sids, rooms}}} = wsServer;
@@ -27,6 +36,10 @@ function publicRooms() {
     return publicRooms;
 }
 
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Poop";
     socket.onAny((event) => {
@@ -35,7 +48,7 @@ wsServer.on("connection", (socket) => {
     });
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => {
-            socket.to(room).emit("bye", socket.nickname);
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
         });
     });
     socket.on("disconnect", () => {
@@ -44,7 +57,7 @@ wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName, showRoom) => {
         socket.join(roomName);
         showRoom();
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
         wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("new_message", (msg, room, done) => {

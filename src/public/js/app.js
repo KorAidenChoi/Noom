7280ae18f08fdb44a012192fe2e725d73f7dd04d
KorAidenChoi <kor.aiden.choi@gmail.com>
2023-01-10 20:16:33 -0800

    camera switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,12 +2,27 @@ const socket = io();
 
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 const myFace = document.getElementById("myFace");
 
 let muted = false;
 let cameraOff = false;
 
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        cameras.forEach((camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        }));
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 let myStream;
 
@@ -18,6 +33,7 @@ async function getMedia() {
             video: true,
         });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch (e) {
         console.log(e);
     }
@@ -26,6 +42,9 @@ async function getMedia() {
 getMedia();
 
 function handleCameraClick(){
+    myStream.getVideoTracks().forEach((track) => {
+        track.enabled = !track.enabled;
+    });
     if (!cameraOff) {
         cameraBtn.innerText = "Camera Off";
         cameraOff = true;
@@ -36,6 +55,9 @@ function handleCameraClick(){
 }
 
 function handleMuteClick(){
+    myStream.getAudioTracks().forEach((track) => {
+        track.enabled = !track.enabled;
+    });
     if (!muted) {
         muteBtn.innerText = "Mute";
         muted = true;

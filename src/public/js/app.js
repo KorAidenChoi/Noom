26fadcfed4d4914a01e21f248c0b7b34634b9a4c
KorAidenChoi <kor.aiden.choi@gmail.com>
2023-01-31 20:13:25 -0800

    stun and cameras

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -88,6 +88,11 @@ function handleMuteClick(){
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find(sender => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -140,7 +145,19 @@ socket.on("ice", (ice) => {
 // RTC Stuff
 
 function makeConnection () {
-    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ],
+            },
+        ],
+    });
     myPeerConnection.addEventListener("icecandidate", handleIce);
     myPeerConnection.addEventListener("addstream", handleAddStream);
     myStream.getTracks()
